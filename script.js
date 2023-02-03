96dc506c118d5b84a19f300e3c3640996ac13f58
Karim <elkheshen.karim@gmail.com>
2023-02-03 09:52:12 +0200

    - Make HTML layout and ID names more readable. - Make layout always vertical (responsiveness now broken) - Experiment with different component color palettes. - In onload(), remove the need for some functions.

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-const PERCOLATION_COLOR_PALETTE = [
-    '#10002b',
-    '#240046',
-    '#3c096c',
-    '#5a189a',
-    '#7b2cbf',
-    '#9d4edd',
-    '#c77dff',
-    '#e0aaff'
-]
+// const COMPONENT_COLOR_PALETTE = ['0d1b2a', '1b263b', '415a77', '778da9', 'e0e1dd']
+const COMPONENT_COLOR_PALETTE = ['0d1b2a', '1b263b', '415a77', '778da9', 'e0e1dd',
+'34495e', '2c3e50', '95a5a6', '7f8c8d', 'bdc3c7',
+'16a085', '27ae60', '2980b9', '8e44ad', '2c3e91']
 
 window.onload = function() {
-    
-    // Get canvas parameters
-    const canvas = document.getElementById("percolation-canvas");
-    const canvasWidth = canvas.clientWidth;
-    const canvasHeight = canvas.clientHeight;
-
-    // Get and set 2D context 
-    const ctx = canvas.getContext('2d', { willReadFrequently : true });
-    ctx.imageSmoothingEnabled = false;
 
     // gather prob control elements & current p value + update control ui
     const probabilityValue = document.getElementById("probability-value");
     const probabilitySlider = document.getElementById("probability-slider");
     let currentProbability = parseFloat(probabilitySlider.value).toFixed(2);
     probabilityValue.innerHTML = `<span class="letterP">P</span>: ${currentProbability}`;
+
+    // Get canvas parameters
+    const canvas = document.getElementById("percolation-canvas");
+    const canvasWidth = canvas.clientWidth;
+    const canvasHeight = canvas.clientHeight;
+
+    // Get and set 2D context
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    ctx.imageSmoothingEnabled = false;
 
     // generate grid and get connected components for current p
     const grid = generateRandomGrid(canvasHeight, canvasWidth);
@@ -34,14 +28,19 @@ window.onload = function() {
     const colorIndex = [];
     const numIndexes = canvasHeight * canvasWidth;
     for (let i = 0; i < numIndexes; i++) {
-        const randomColor = generateRandomColorFromPalette();
-        colorIndex.push(randomColor);
+        const randIndex = Math.floor(Math.random() * COMPONENT_COLOR_PALETTE.length);
+        const randomColor = COMPONENT_COLOR_PALETTE[randIndex];
+        colorIndex.push({
+            r: Math.floor(parseInt(randomColor.substring(0, 2), 16)),
+            g: Math.floor(parseInt(randomColor.substring(2, 4), 16)),
+            b: Math.floor(parseInt(randomColor.substring(4, 6), 16))
+        });
     }
 
     // create cache and pre-store some frames
     cachedComponents = {};
     cachedComponents[currentProbability] = currentComponents;
-    for (let i = 45; i <= 80; i++) {
+    for (let i = 0; i <= 100; i += 4) {
         const prob = (i / 100).toFixed(2);
         cachedComponents[prob] = getConnectedComponents(grid, prob);
     }
@@ -49,11 +48,11 @@ window.onload = function() {
     drawComponentsToCanvas(currentComponents, colorIndex, ctx, canvasHeight, canvasWidth);
 
     probabilitySlider.oninput = function () {
-        
+
         // reset control ui and grab new value of p
         probabilityValue.innerHTML = `<span class="letterP">P</span>: ${currentProbability}`;
         currentProbability = parseFloat(probabilitySlider.value).toFixed(2);
-        
+
         // draw current components and store component list if not already cached
         if (currentProbability in cachedComponents) {
             drawComponentsToCanvas(cachedComponents[currentProbability], colorIndex, ctx, canvasHeight, canvasWidth);
@@ -173,31 +172,31 @@ function generateRandomColorFromPalette() {
 function generateRandomColor() {
     return {
         r: Math.floor(Math.random() * 256),
-        g: Math.floor(Math.random() * 256), 
+        g: Math.floor(Math.random() * 256),
         b: Math.floor(Math.random() * 256)
     };
 }
 
 
 /* Sets up the frame of connected components with their assigned colors related to the current value of P, and draws it to the canvas. */
-function drawComponentsToCanvas(componentList, colorIndex, ctx, canvasHeight, canvasWidth) {    
-    
+function drawComponentsToCanvas(componentList, colorIndex, ctx, canvasHeight, canvasWidth) {
+
     const imgData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
     for (let i = 0; i < componentList.length; i++) {
         const currComponent = componentList[i];
         const currMinIndex = currComponent[0];
-        
-        const currColor = colorIndex[currMinIndex];
+
+        let currColor = colorIndex[currMinIndex];
         if (currColor === undefined) {
             continue;
         }
-        
+
         for (let j = 0; j < currComponent.length; j++) {
             const currCoord = indexToCoord(currComponent[j], canvasWidth);
             const x = currCoord[0];
             const y = currCoord[1];
-    
+
             const index = (y * canvasWidth + x) * 4;
             imgData.data[index + 0] = currColor.r;
             imgData.data[index + 1] = currColor.g;
@@ -205,7 +204,7 @@ function drawComponentsToCanvas(componentList, colorIndex, ctx, canvasHeight, ca
             imgData.data[index + 3] = 255;
         }
     }
-    
+
     ctx.putImageData(imgData, 0, 0);
 }
 
